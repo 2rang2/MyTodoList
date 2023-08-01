5c814285443cb884f898d29e29d7e7b2cc127c46
2rang2 <rang0825@gmail.com>
2023-08-01 19:27:27 +0900

    Initial Commit

---
+++ My Todo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Todo List
+//
+//  Created by t2023-m0046 on 2023/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
